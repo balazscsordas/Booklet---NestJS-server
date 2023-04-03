26bba6ea66a6706af7386b8b3ce87bd9c2b1012b
Csordas Balazs <csordasbalu96@gmail.com>
2023-04-03 11:01:08 +0200

    removed swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
@@ -11,13 +10,6 @@ async function bootstrap() {
     credentials: true,
     origin: [process.env.CLIENT_URL],
   });
-
-  const config = new DocumentBuilder()
-    .setTitle('Language App')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT || 3001);
 }
