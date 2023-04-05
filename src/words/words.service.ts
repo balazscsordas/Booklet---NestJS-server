7b2b9d2acb2a9d4ac75e83b4026700b69adcfa10
Csordas Balazs <csordasbalu96@gmail.com>
2023-04-05 15:03:26 +0200

    few changes

--- src/words/words.service.ts
+++ src/words/words.service.ts
@@ -3,14 +3,14 @@ import {
   HttpStatus,
   Injectable,
   NotFoundException,
-} from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { QueryFailedError, Repository } from 'typeorm';
-import { AddNewWordDto } from './dto/AddNewWord.dto';
-import { EditWordDto } from './dto/EditWord.dto';
-import { Word } from './word.entity';
-import { GetRandomWordDto } from './dto/GetRandomWord.dto';
-import { WordFormatter } from './helper/word-formatter';
+} from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { QueryFailedError, Repository } from 'typeorm'
+import { AddNewWordDto } from './dto/AddNewWord.dto'
+import { EditWordDto } from './dto/EditWord.dto'
+import { Word } from './word.entity'
+import { GetRandomWordDto } from './dto/GetRandomWord.dto'
+import { WordFormatter } from './helper/word-formatter'
 
 @Injectable()
 export class WordsService {
@@ -25,20 +25,20 @@ export class WordsService {
         .select()
         .where({ user_id: userId })
         .orderBy('RAND()')
-        .getOne();
+        .getOne()
       if (randomWord == null) {
-        throw new HttpException('Word list is empty', HttpStatus.NO_CONTENT);
+        throw new HttpException('Word list is empty', HttpStatus.NO_CONTENT)
       }
-      const formattedWord = WordFormatter(randomWord, quizSettings);
-      return formattedWord;
+      const formattedWord = WordFormatter(randomWord, quizSettings)
+      return formattedWord
     } catch (err) {
       if (err.status == 204) {
-        throw new HttpException('Word list is empty', HttpStatus.NO_CONTENT);
+        throw new HttpException('Word list is empty', HttpStatus.NO_CONTENT)
       } else {
         throw new HttpException(
           `Serverside error occured.`,
           HttpStatus.INTERNAL_SERVER_ERROR,
-        );
+        )
       }
     }
   }
@@ -51,19 +51,19 @@ export class WordsService {
         .where({ user_id: userId })
         .skip((page - 1) * 20)
         .take(20)
-        .getMany();
+        .getMany()
       if (words.length === 0 || !words) {
-        throw new HttpException('Havent added any word', HttpStatus.NO_CONTENT);
+        throw new HttpException('Havent added any word', HttpStatus.NO_CONTENT)
       }
-      return words;
+      return words
     } catch (err) {
       if (err.status == 204) {
-        throw new HttpException('Word list is empty', HttpStatus.NO_CONTENT);
+        throw new HttpException('Word list is empty', HttpStatus.NO_CONTENT)
       } else {
         throw new HttpException(
           `Serverside error occured.`,
           HttpStatus.INTERNAL_SERVER_ERROR,
-        );
+        )
       }
     }
   }
@@ -72,50 +72,55 @@ export class WordsService {
     try {
       const numberOfWords = await this.wordRepository.count({
         where: { user_id: userId },
-      });
+      })
       if (numberOfWords === 0) {
-        return 1;
+        return 1
       }
-      const maxPageNumber = Math.ceil(numberOfWords / 20);
-      console.log(maxPageNumber);
-      return maxPageNumber;
+      const maxPageNumber = Math.ceil(numberOfWords / 20)
+      console.log(maxPageNumber)
+      return maxPageNumber
     } catch (err) {
-      console.log(err);
+      console.log(err)
       throw new HttpException(
         `Serverside error occured.`,
         HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+      )
     }
   }
 
   async getLanguageOptions(userId: number) {
     try {
-      const languageOptions = ['hun', 'eng'];
-      return languageOptions;
+      const languageOptions = ['hun', 'eng']
+      return languageOptions
     } catch (err) {
-      console.log(err);
+      console.log(err)
     }
   }
 
   async getOneById(id: number) {
     try {
-      const word = await this.wordRepository.findOne({ where: { id } });
+      const word = await this.wordRepository.findOne({ where: { id } })
       if (!word) {
-        throw new NotFoundException();
+        throw new NotFoundException()
       }
-      return word;
+      const returnedWord = {
+        id: word.id,
+        hun: word.hun,
+        eng: word.eng,
+      }
+      return returnedWord
     } catch (err) {
-      console.log(err);
+      console.log(err)
       if (err.status == 404) {
         throw new HttpException(
           `There isnt any user with id: ${id}`,
           HttpStatus.NOT_FOUND,
-        );
+        )
       } else {
         throw new HttpException(
           `Serverside error occured.`,
           HttpStatus.INTERNAL_SERVER_ERROR,
-        );
+        )
       }
     }
   }
@@ -125,20 +130,20 @@ export class WordsService {
       const newWord = this.wordRepository.create({
         ...newWordData,
         user_id: userId,
-      });
-      const savedWord = await this.wordRepository.save(newWord);
+      })
+      const savedWord = await this.wordRepository.save(newWord)
     } catch (err) {
-      console.log(err);
+      console.log(err)
       if (err instanceof QueryFailedError) {
         throw new HttpException(
           `Data validation failed while saving the new word to the database.`,
           HttpStatus.NOT_IMPLEMENTED,
-        );
+        )
       } else {
         throw new HttpException(
           'Serverside error occured',
           HttpStatus.INTERNAL_SERVER_ERROR,
-        );
+        )
       }
     }
   }
@@ -148,39 +153,39 @@ export class WordsService {
       const word = await this.wordRepository.update(
         { id: newWordData.id },
         { ...newWordData, user_id: userId },
-      );
+      )
       const editedWord = await this.wordRepository.findOne({
         where: { id: newWordData.id },
-      });
-      return editedWord;
+      })
+      return editedWord
     } catch (err) {
-      console.log(err);
+      console.log(err)
       if (err instanceof QueryFailedError) {
         throw new HttpException(
           `Data validation failed while saving the new word to the database.`,
           HttpStatus.NOT_IMPLEMENTED,
-        );
+        )
       } else {
         throw new HttpException(
           'Serverside error occured',
           HttpStatus.INTERNAL_SERVER_ERROR,
-        );
+        )
       }
     }
   }
 
   async deleteOneById(id: number) {
     try {
-      const deletedWord = await this.wordRepository.delete({ id });
+      const deletedWord = await this.wordRepository.delete({ id })
       if (deletedWord.affected < 1 || !deletedWord) {
-        throw new NotFoundException();
+        throw new NotFoundException()
       }
     } catch (err) {
-      console.log(err);
+      console.log(err)
       throw new HttpException(
         'Serverside error occured',
         HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+      )
     }
   }
 }
